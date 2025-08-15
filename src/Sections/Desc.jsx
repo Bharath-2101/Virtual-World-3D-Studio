b81f7654cd913a8b1d1c29eb6f09dc37f45cfc5d
Bharath <bharthkamatham143@gmail.com>
2025-08-15 13:41:43 +0530

    minor Changes

--- src/Sections/Desc.jsx
+++ src/Sections/Desc.jsx
@@ -7,7 +7,7 @@ const Desc = () => {
   useTitle(titleRef, "[ The Craft of Digital ]");
 
   return (
-    <section className="h-screen w-screen grid grid-rows-[3fr_9fr_1fr] px-[5vw] font-[Space] lg:mt-[20vh]">
+    <section className="h-screen w-screen grid grid-rows-[3fr_9fr_1fr] px-[5vw] font-[Space] lg:mt-[20vh] gap-3">
       <div className="flex flex-col gap-1 justify-center">
         <span
           ref={titleRef}
@@ -21,9 +21,9 @@ const Desc = () => {
         </span>
       </div>
 
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center overflow-hidden rounded-2xl bg-red-200">
         <div
-          className="h-[90%] w-[90%] rounded-md bg-cover bg-center bg-no-repeat"
+          className="h-[100%] w-[100%] rounded-md bg-cover bg-center bg-no-repeat hover:scale-110 ease-[cubic-bezier(0.76, 0, 0.24, 1)] duration-500 rounded-2xl"
           style={{ backgroundImage: `url("/images/Robot.png")` }}
         />
       </div>
